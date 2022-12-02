0f18607fc1002f2adee626b207c8a9238f360b4b
BeatrizMiotto <miottobiasforever@gmail.com>
2022-12-02 12:17:35 -0300

    incluindo CRUD em pedidos e pedidosProdutos

--- controllers/pedidosControllers.js
+++ controllers/pedidosControllers.js
@@ -4,5 +4,31 @@ module.exports={
 index: async (req, res, next)=> {
     let pedidos = await Pedido.listaPedidos()
     res.status(200).send(pedidos);
-  }};
+  },
+  create: (req, res, next) => {
+    const pedido = new Pedido(req.body)
+    pedido.id = new Date().getTime()
+    Pedido.salvar(pedido)
+    res.status(201).send(pedido)
+  },
+  delete: (req, res, next) => {
+    Pedido.apagarPorId(req.params.id)
+    res.status(204).send("")
+  },
+  update: async (req, res, next) => {
+    let pedidoDb = await Pedido.buscarPorId(req.params.id)
+    if (!pedidoDb) return res.status(404).send({ mensagem: "Pedido não encontrado" })
+
+    const pedido = new Pedido(req.body)
+    pedido.id = pedidoDb.id
+    Pedido.salvar(pedido)
+    res.status(200).send(pedido)
+  },
+  show: async (req, res, next) => {
+    let pedidoDb = await Pedido.buscarPorId(req.params.id)
+    if (!pedidoDb) return res.status(404).send({ mensagem: "Pedido não encontrado" })
+    res.status(200).send(pedidoDb)
+  }
+
+};
   
--- controllers/pedidosProdutosControllers.js
+++ controllers/pedidosProdutosControllers.js
@@ -4,4 +4,30 @@ module.exports={
 index: async (req, res, next)=> {
     let pedidosProdutos = await PedidoProduto.listaPedidosProdutos()
     res.status(200).send(pedidosProdutos);
-  }};
+  },
+  create: (req, res, next) => {
+    const pedidoProduto = new PedidoProduto(req.body)
+    pedidoProduto.id = new Date().getTime()
+    PedidoProduto.salvar(pedidoProduto)
+    res.status(201).send(pedidoProduto)
+  },
+  delete: (req, res, next) => {
+    PedidoProduto.apagarPorId(req.params.id)
+    res.status(204).send("")
+  },
+  update: async (req, res, next) => {
+    let pedidoProdutoDb = await PedidoProduto.buscarPorId(req.params.id)
+    if (!pedidoProdutoDb) return res.status(404).send({ mensagem: "Não encontrado" })
+
+    const pedidoProduto = new PedidoProduto(req.body)
+    pedidoProduto.id = pedidoProdutoDb.id
+    PedidoProduto.salvar(pedidoProduto)
+    res.status(200).send(pedidoProduto)
+  },
+  show: async (req, res, next) => {
+    let pedidoProdutoDb = await PedidoProduto.buscarPorId(req.params.id)
+    if (!pedidoProdutoDb) return res.status(404).send({ mensagem: "Não encontrado" })
+    res.status(200).send(pedidoProdutoDb)
+  }
+
+};
