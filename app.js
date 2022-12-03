f7903c72a92d26cb7cba1333cf1257a485f2f22a
Bruno Ernandes da Silva <48297443+bruno-esilva@users.noreply.github.com>
2022-12-03 07:00:22 -0300

    Populados dados das DBs pedidos e produtosPedidos

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'http://localhost:4200'
+  origin: '*'
 }));
 
 app.use('/', indexRouter);
