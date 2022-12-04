4da7c6be238276f2024f7d33d7ee6730201a00da
BeatrizMiotto <miottobiasforever@gmail.com>
2022-12-03 22:05:47 -0300

    CRUD PedidosProdutos

--- models/pedidosProdutos.js
+++ models/pedidosProdutos.js
@@ -19,4 +19,58 @@ module.exports = class PedidoProduto {
   
       return pedidosProdutos
     }
+    static async buscarPorId(id) {
+      const listaPedidosProdutos = await this.listaPedidosProdutos()
+      for (let i = 0; i < listaPedidosProdutos.length; i++) {
+          const pedidosProdutosDb = listaPedidosProdutos[i]
+          if (pedidosProdutosDb.id.toString() === id.toString()) {
+              return pedidosProdutosDb
+          }
+      }
+
+      return null
+  }
+  static async salvar(pedidosProdutos) {
+    const listaPedidosProdutos = await this.listaPedidosProdutos()
+    let exist = false
+    for (let i = 0; i < listaPedidosProdutos.length; i++) {
+        const pedidosProdutosDb = listaPedidosProdutos[i]
+        if (pedidosProdutosDb.id.toString() === pedidosProdutos.id.toString()) {
+            pedidosProdutosDb.pedido_id = pedidosProdutos.pedido_id
+            pedidosProdutosDb.produto_id = pedidosProdutos.produto_id
+            pedidosProdutosDb.valor = pedidosProdutos.valor
+            pedidosProdutosDb.quantidade = pedidosProdutos.quantidade
+            exist = true
+            break
+          }
+        }
+
+    if (!exist) {
+        const objectLiteral = { ...pedidosProdutos }
+        listaPedidosProdutos.push(objectLiteral)
+    }
+
+    PedidoProduto.salvarJsonDisco(listaPedidosProdutos)
+}
+static async salvarJsonDisco(pedidosProdutos) {
+  const fs = require('fs');
+
+  try {
+      fs.writeFileSync('dataBase_G3/pedidosProdutos.json', JSON.stringify(pedidosProdutos), { encoding: "utf8" });
+  } catch (err) {
+      console.error(err);
+  }
+}
+static async apagarPorId(id) {
+  const listaPedidosProdutos = await this.listaPedidosProdutos()
+  const novoPedidosProdutos = []
+  for (let i = 0; i < listaPedidosProdutos.length; i++) {
+      const pedidosProdutosDb = listaPedidosProdutos[i]
+      if (pedidosProdutosDb.id.toString() !== id.toString()) {
+        novoPedidosProdutos.push(pedidosProdutosDb)
+      }
+  }
+
+  PedidoProduto.salvarJsonDisco(novoPedidosProdutos)
+}
   }
